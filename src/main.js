8189ee45aeaef08501bf921a03f94472bf48ad71
Michal Schiller <mschiller890@gmail.com>
2026-07-25 15:17:44 +0200

    Added fetching APOD and some styling

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './style.css'
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 
-document.querySelector('#app').innerHTML = `
-
-`
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
+  document.body.style.backgroundImage = `url(${data.url})`;
+});
